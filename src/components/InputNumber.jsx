e0d73ec0e7519e8992e2665b22abfe6e4595c10c
Vijay Bansal <134121986+VijayBansal2004@users.noreply.github.com>
2024-09-01 15:05:15 +0000

    resolved enter button sizing and spacing

--- src/components/InputNumber.jsx
+++ src/components/InputNumber.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const InputNumber = ({ number, setNumber, handleSubmit }) => {
   return (
-    <form className="row g-3">
-      <div className="col-12">
+    <form className="row g-3 mb-3">
+      <div className="col-12 col-sm-9">
         <input
           type="number"
           className="user-input-number"
@@ -12,11 +12,12 @@ const InputNumber = ({ number, setNumber, handleSubmit }) => {
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
+      </div>
+      <div className="col-12 col-sm-3">
         <button
           type="submit"
-          className={`btn btn-success mx-2 enter-button ${
-            number == null && "disabled"
-          }`}
+          className={`btn btn-success enter-button ${number == null && "disabled"
+            }`}
           onClick={(e) => handleSubmit(e)}>
           Enter
         </button>
